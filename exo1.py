af47550cc58c5f22001ebcacbc28618a342eba67
Ilyass0802 <ilyasselalami08@gmail.com>
2025-01-21 17:07:33 -0500

    exo1

--- exo1.py
+++ exo1.py
@@ -1,9 +1,13 @@
 # Demander le nom complet de l'utilisateur
+nom = input("Veuillez entrer votre nom complet : ")
+print( )
 # Demander l'âge de l'utilisateur
-print('helgfsdfsdgsflo')
+âge = input("Veyillez entrer votre âge : ")
+print(a)
 # Définir l'année actuelle
+
 # Calculer l'année de naissance
-pritn ("gg")
+
 # Afficher un message de bienvenue avec le nom complet
+
 # Afficher l'année de naissance
-Print ("allo seb")
