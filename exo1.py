175385d6d36985145fc9a51c19e7fb6984a65ffd
SebGagnon <sebasgagnon95@gmail.com>
2025-01-23 14:47:01 -0500

    Exo1 fini, passe tous les tests

--- exo1.py
+++ exo1.py
@@ -1,13 +1,14 @@
 # Demander le nom complet de l'utilisateur
 nom = input("Veuillez entrer votre nom complet : ")
-print( )
+
 # Demander l'âge de l'utilisateur
-âge = input("Veyillez entrer votre âge : ")
-print(a)
+âge = input("Veuillez entrer votre âge : ")
+
 # Définir l'année actuelle
-
+annee_actuelle = 2025
 # Calculer l'année de naissance
-
+annee_naissance = annee_actuelle - int(âge)
 # Afficher un message de bienvenue avec le nom complet
-
+print(f"Bonjour {nom}")
+print(f"Vous êtes né(e) en {annee_naissance}")
 # Afficher l'année de naissance
