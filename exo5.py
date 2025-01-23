707735ecc7d0bc56e07977133d47e5d1f5ec86e9
SebGagnon <sebasgagnon95@gmail.com>
2025-01-23 15:08:44 -0500

    Fini exo5, tous les tests du lab passent

--- exo5.py
+++ exo5.py
@@ -5,15 +5,15 @@ g = 9.8
 
 # Demander la vitesse initiale de la boule
 # Demander la vitesse initiale de la boule
-
+v0 = input("Vitesse initiale (en m/s): ")
 # Demander l'angle de lancement
-
+Theta = input("Angle de lancer (en degrés): ")  
 # Convertir l'angle en radians
-
+theta = math.radians(float(Theta))  
 # Calculer la distance maximale en x
-
+distance = (float(v0)**2)*math.sin(2*theta)/g   
 # Afficher la distance maximale arrondie à 2 chiffres après la virgule
-
+print(f"Distance parcourue: {distance:.2f}m")
 # Exemple:
 # Pour une vitesse initiale de 10 m/s et un angle de 45 degrés
 # La distance parcourue serait de 10.20m
